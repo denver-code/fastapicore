c5d33ea3c4a5285b589e5a7a665ea43dc47fc1ef
Denver <csigorek@gmail.com>
2021-11-14 16:56:42 +0200

    added v1 api

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
 
-from typing import Optional
+import app.v1 as v1
 
 app = FastAPI(title="FastAPICore", debug=True)
 
@@ -25,6 +25,4 @@ async def startup():
     redis = await aioredis.create_redis_pool("redis://localhost")
     await FastAPILimiter.init(redis)
 
-@app.get("/", dependencies=[Depends(RateLimiter(times=2, seconds=5))])
-async def home_event():
-    return {"Hello":"world"}
+app.include_router(v1.api, prefix="/api")
---
+++ app/v1/public/__init__.py
@@ -0,0 +1,5 @@
+from fastapi import (
+    APIRouter,
+)
+
+public = APIRouter(prefix="/public")
